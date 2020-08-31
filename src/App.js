c6568808405e52a75cd599f186efc9c130a1497b
Oskar Tengberg <52031625+MrTengil@users.noreply.github.com>
2020-08-31 14:58:00 +0200

    Time

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import Header from "./components/Header";
 import "./style.css";
 
 function App() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
   return (
     <div>
       <Header />
+      <h1>Good {timeOfDay}!</h1>
       <MainContent />
       <Footer />
     </div>
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 function ToDo() {
+  const itemName = "Item";
   return (
     <div>
       <input type="checkbox" />
-      <p>Item</p>
+      <p>{itemName}</p>
     </div>
   );
 }
