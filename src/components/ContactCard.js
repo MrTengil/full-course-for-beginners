b62fc2bd9b393a47e67de256b7974c8836d52455
Oskar Tengberg <52031625+MrTengil@users.noreply.github.com>
2020-09-02 16:24:17 +0200

    Update ContactCard.js

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -3,7 +3,7 @@ import React from "react";
 function ContactCard(props) {
   return (
     <div>
-      <img src={props.imgUrl} alt={`${props.name}`} />
+      <img src={props.imgUrl} alt={props.name} />
       <h3>Name: {props.name} </h3>
       <p>Phone: {props.phone}</p>
       <p>Email: {props.email}</p>
